4a29d5053d4c937e7626275bc3e4d9e87ae1994a
mibkvr <40341638+mibkvr@users.noreply.github.com>
2020-03-30 18:07:13 +0200

    Try to fix login WIP

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,100 +1,73 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './Login.scss';
-import eye from '../../assets/icons/eye.svg';
-import eyeOff from '../../assets/icons/eye-off.svg';
+import EyeOffIcon from '../icons/eye-off.icon';
+import EyeIcon from '../icons/eye.icon';
 
 class Login extends Component {
-    state = { email: '', password: '' };
 
-    handleEmailChange = (event) => {
-        this.setState({ email: event.target.value });
+    state = { 
+        email: '',
+        password: '',
+        passwordHidden: true
     };
 
-    handlePasswortChange = (event) => {
-        this.setState({ password: event.target.value });
+    togglePasswordHide() {
+        this.state.passwordHidden = !this.state.passwordHidden;
+        this.setState(this.state);
+    }
+
+    emailChangeListener = (event) => {
+        this.state.email = event.target.value;
+    };
+
+    passwordChangeListener = (event) => {
+        this.state.password = event.target.value;
     };
 
     handleSubmit = (event) => {
         event.preventDefault();
-
-        const user = { email: this.state.email, password: this.state.password };
-
-        const response = axios.post(`https://jsonplaceholder.typicode.com/users`, { user }).then(
-            (res) => {
-                /* axios.post(`https://user.e-edu.the-morpheus.de/users/login`, {user}).then(res => { */
-            },
-            (onerror) => {}
-        );
-        if (response) {
-            this.props.history.push('/dashboard');
-        }
     };
-
-    /** Method to switch between visibility and non-visibility
-     * @public
-     */
-    changeVisibility() {
-        const passwortType = document.getElementById('password');
-        const hiddenEye = document.getElementById('hide1');
-        const openEye = document.getElementById('hide2');
-
-        if (passwortType.type === 'password') {
-            passwortType.type = 'text';
-            hiddenEye.style.display = 'block';
-            openEye.style.display = 'none';
-        } else {
-            passwortType.type = 'password';
-            hiddenEye.style.display = 'none';
-            openEye.style.display = 'block';
-        }
-    }
 
     render() {
         return (
             <div className="login-component">
-                {/* describes the login-popup */}
-                <form className="box" onSubmit={this.handleSubmit} action="loginPop.html" method="post">
-                    <div className="boxinhalt">
-                        <h1 className="headline">L O G I N</h1>
-                        {/* describes the email-input-box */}
-                        <div className="input-box">
-                            <input
-                                type="text"
-                                name="email"
-                                placeholder="Enter Email..."
-                                className="input-field"
-                                required
-                                onChange={this.handleEmailChange}
-                            />
-                        </div>
-                        {/* describes the password-input-box */}
-                        <div className="input-box">
-                            <input
-                                type="password"
-                                name="passwort"
-                                placeholder="Enter Password..."
-                                className="input-field passwort"
-                                required
-                                onChange={this.handlePasswortChange}
-                            />
-                            {/* describes the two eye-icons in the field */}
-                            <span className="eye" onClick={this.changeVisibility}>
-                                <img className="hide1" src={eye} />
-                                <img className="hide2" src={eyeOff} />
-                            </span>
-                        </div>
-                        {/* describes the google-login-field */}
-                        <button className="pointer google-login">
-                            <img
-                                className="google-bild"
-                                src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
-                            />
-                            Log In with Google
-                        </button>
-                        <input type="submit" className="login button pointer" value="Log In" />
+                <h1 className="login-title">L O G I N</h1>
+                {/* describes the email-input-box */}
+                <div className="input-box">
+                    <input
+                        type="text"
+                        placeholder="Enter Email..."
+                        className="input-field"
+                        onChange={this.emailChangeListener}
+                    />
+                </div>
+                {/* describes the password-input-box */}
+                <div className="input-box">
+                    <input
+                        type="password"
+                        placeholder="Enter password..."
+                        className="input-field passwort"
+                        onChange={this.passwordChangeListener}
+                    />
+                    {/* describes the two eye-icons in the field */}
+                    <div className="showPasswordButton clickable" onClick={this.togglePasswordHide}>
+                    {
+                        this.state.passwordHidden ?
+                        <EyeOffIcon/> : <EyeIcon/>
+                    }
                     </div>
-                </form>
+                </div>
+                {/* describes the google-login-field */}
+                <button className="clickable google-login">
+                    <img
+                        className="google-pic"
+
+                        src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+                    />
+                    Log In with Google
+                </button>
+                <button className="clickable" onClick={this.handleSubmit}>Login</button>
             </div>
         );
     }
